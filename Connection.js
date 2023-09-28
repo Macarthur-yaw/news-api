a036fb2459ef16093c483bca0c6a2f2cb3abb48a
Kevin <arthurkevin1260@gmail.com>
2023-09-29 00:28:30 +0100

    changes done

--- Connection.js
+++ Connection.js
@@ -1,7 +1,6 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
 
-const url="mongodb+srv://arthurkevin1260:Zl7rH2aqRTOvfMf3@cluster0.d4rjoj9.mongodb.net/?retryWrites=true&w=majority"
 
 const mongoURI=process.env.MONGODB_URI;
 mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology: true}).then(()=>{
